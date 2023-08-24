aadea29754177618206d1d50261a5a8b8c6644f1
Theomrh <92977368+Theomrh@users.noreply.github.com>
2023-08-24 21:41:53 +0200

    update header component and make profile private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,29 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <div className="bg-slate-200">
-        <div className="flex items-center justify-between max-w-6xl p-3 mx-auto">
-            <Link to="/"><h1 className="font-bold">MERN PROJECT</h1></Link>
-            <ul className="flex gap-4">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/sign-in">Sign In</Link></li>
-            </ul>
-        </div>
+      <div className="flex items-center justify-between max-w-6xl p-3 mx-auto">
+        <Link to="/">
+          <h1 className="font-bold">MERN PROJECT</h1>
+        </Link>
+        <ul className="flex gap-4">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/profile">
+              {currentUser ? <p>Profil</p> : <p>Sign In</p>}
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
